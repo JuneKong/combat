1f968188da466b7471712bec7e600f06e7d9f63f
June <741527390@qq.com>
2018-11-15 17:41:13 +0800

    add config

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# name: 配置文件--开发
+
+
+configs = {
+	'debug': True,
+	'db': {
+		'host': '127.0.0.1',
+		'post': 3306,
+		'user': 'root',
+		'password': 'root',
+		'db': 'combat'
+	},
+	'session': {
+		'secret': 'Combat'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# name: 配置文件--发布
+
+configs = {
+	'db': {
+		'host': '127.0.0.1'
+	}
+}
